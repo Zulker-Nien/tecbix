3930f9a82126b720b836194ec05e32a3eb52aa3c
Zulker Nien <zulkerb9b@gmail.com>
2022-09-12 13:25:25 +0600

    Loader added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
+import Loader from "./Components/Loader";
 import Home from "./Pages/Home";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [fetching, setFetching] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setFetching(false);
+    }, 3000);
+  });
   return (
-    <div className="mainContainer">
-      <Navbar />
-      <Home />
-
-      <Footer />
-    </div>
+    <>
+      {fetching ? (
+        <Loader />
+      ) : (
+        <div className="mainContainer">
+          <Navbar />
+          <Home />
+          <Footer />
+        </div>
+      )}
+    </>
   );
 }
 
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,13 +8,14 @@ import infoimg4 from "../Media/infoimg4.png";
 import infoimg5 from "../Media/infoimg5.png";
 import Button from "../Components/Button";
 import HomeBgImage from "../Components/HomeBgImage";
+
 import { useMediaQuery } from "react-responsive";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { Pagination, Navigation } from "swiper";
+import { Pagination, Navigation, Autoplay } from "swiper";
 import "./Home.scss";
 
 import emailjs from "@emailjs/browser";
@@ -45,140 +46,143 @@ function Home() {
   };
 
   return (
-    <div className="homeContainer">
-      <div className="intro">
-        <img src={introimg} alt={""}></img>
-        <div className="slogan">
-          <div className="name">
-            <h1>TECBIX</h1>
-          </div>
-          <div className="headline">
-            <h5>IT Enabled Services</h5>
-          </div>
-          <div className="catchphrase">
-            <h3>Building the future,</h3>
-            <h3>One innovation at a time.</h3>
+    <>
+      <div className="homeContainer">
+        <div className="intro">
+          <img src={introimg} alt={""}></img>
+          <div className="slogan">
+            <div className="name">
+              <h1>TECBIX</h1>
+            </div>
+            <div className="headline">
+              <h5>IT Enabled Services</h5>
+            </div>
+            <div className="catchphrase">
+              <h3>Building the future,</h3>
+              <h3>One innovation at a time.</h3>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="aboutUs">
-        <div className="homeSwirlBackground">
-          <HomeBgImage />
+        <div className="aboutUs">
+          <div className="homeSwirlBackground">
+            <HomeBgImage />
+          </div>
+          <h1>Get to know us</h1>
+          <div className="line"></div>
+          <p>
+            A tech-driven company that helps you make sense of your data and get
+            results. With a dedicated team of experts, we strive to be your most
+            reliable technology partner. Tecbix is built on a foundation of
+            innovation and quality. We don't just talk about being a flexible
+            partner for your business; we live
+          </p>
         </div>
-        <h1>Get to know us</h1>
-        <div className="line"></div>
-        <p>
-          A tech-driven company that helps you make sense of your data and get
-          results. With a dedicated team of experts, we strive to be your most
-          reliable technology partner. Tecbix is built on a foundation of
-          innovation and quality. We don't just talk about being a flexible
-          partner for your business; we live
-        </p>
-      </div>
-      <div className="services">
-        <h1>What we do</h1>
-        <div className="line"></div>
-        <p>
-          A tech-driven company that helps you make sense of your data and get
-          results. With a dedicated team of experts, we strive to be your most
-          reliable technology partner. Tecbix is built on a foundation of
-          innovation and quality. We don't just talk about being a flexible
-          partner for your business; we live
-        </p>
-        {isTabletOrMobile ? (
-          <Swiper
-            slidesPerView={1}
-            spaceBetween={30}
-            loop={true}
-            pagination={{
-              clickable: true,
-            }}
-            navigation={true}
-            modules={[Pagination, Navigation]}
-            className="serviceinfo"
-          >
-            <SwiperSlide>
+        <div className="services">
+          <h1>What we do</h1>
+          <div className="line"></div>
+          <p>
+            A tech-driven company that helps you make sense of your data and get
+            results. With a dedicated team of experts, we strive to be your most
+            reliable technology partner. Tecbix is built on a foundation of
+            innovation and quality. We don't just talk about being a flexible
+            partner for your business; we live
+          </p>
+          {isTabletOrMobile ? (
+            <Swiper
+              slidesPerView={1}
+              spaceBetween={30}
+              loop={true}
+              pagination={{
+                clickable: true,
+              }}
+              autoplay={Autoplay}
+              navigation={true}
+              modules={[Pagination, Navigation]}
+              className="serviceinfo"
+            >
+              <SwiperSlide>
+                <div className="info">
+                  <img src={infoimg1} alt={""}></img>
+                  <h6>Startup Setup</h6>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="info">
+                  <img src={infoimg2} alt={""}></img>
+                  <h6>HR Services</h6>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="info">
+                  <img src={infoimg3} alt={""}></img>
+                  <h6>Outsourcing</h6>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="info">
+                  <img src={infoimg4} alt={""}></img>
+                  <h6>Web Development</h6>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="info">
+                  <img src={infoimg5} alt={""}></img>
+                  <h6>Marketing</h6>
+                </div>
+              </SwiperSlide>
+            </Swiper>
+          ) : (
+            <div className="serviceinfo">
               <div className="info">
                 <img src={infoimg1} alt={""}></img>
                 <h6>Startup Setup</h6>
               </div>
-            </SwiperSlide>
-            <SwiperSlide>
               <div className="info">
                 <img src={infoimg2} alt={""}></img>
-                <h6>Startup Setup</h6>
+                <h6>HR Services</h6>
               </div>
-            </SwiperSlide>
-            <SwiperSlide>
               <div className="info">
                 <img src={infoimg3} alt={""}></img>
-                <h6>Startup Setup</h6>
+                <h6>Outsourcing</h6>
               </div>
-            </SwiperSlide>
-            <SwiperSlide>
               <div className="info">
                 <img src={infoimg4} alt={""}></img>
-                <h6>Startup Setup</h6>
+                <h6>Web Development</h6>
               </div>
-            </SwiperSlide>
-            <SwiperSlide>
               <div className="info">
                 <img src={infoimg5} alt={""}></img>
-                <h6>Startup Setup</h6>
+                <h6>Marketing</h6>
               </div>
-            </SwiperSlide>
-          </Swiper>
-        ) : (
-          <div className="serviceinfo">
-            <div className="info">
-              <img src={infoimg1} alt={""}></img>
-              <h6>Startup Setup</h6>
             </div>
-            <div className="info">
-              <img src={infoimg2} alt={""}></img>
-              <h6>HR Services</h6>
-            </div>
-            <div className="info">
-              <img src={infoimg3} alt={""}></img>
-              <h6>Outsourcing</h6>
-            </div>
-            <div className="info">
-              <img src={infoimg4} alt={""}></img>
-              <h6>Web Development</h6>
-            </div>
-            <div className="info">
-              <img src={infoimg5} alt={""}></img>
-              <h6>Marketing</h6>
-            </div>
+          )}
+          <div className="serviceButton">
+            <Button label="View all Services" />
           </div>
-        )}
-        <div className="serviceButton">
-          <Button label="View all Services" />
+        </div>
+        <div className="contactUs">
+          <div className="homeSwirlBackgroundContact">
+            <HomeBgImage />
+          </div>
+          <div className="contactform">
+            <h1>GET IN TOUCH</h1>
+            <div className="line"></div>
+            <form className="form" ref={form} onSubmit={sendEmail}>
+              <input placeholder="Your Name" type="name" name="name" />
+              <input placeholder="Your Email" type="email" name="email" />
+              <textarea
+                placeholder="Your Query"
+                rows={8}
+                type="text"
+                name="message"
+              />
+              {/* <div className="contactButton"> */}
+              <Button label="Submit" type="submit" value="Send" />
+              {/* </div> */}
+            </form>
+          </div>
         </div>
       </div>
-      <div className="contactUs">
-        <div className="homeSwirlBackgroundContact">
-          <HomeBgImage />
-        </div>
-        <div className="contactform">
-          <h1>GET IN TOUCH</h1>
-          <div className="line"></div>
-          <form className="form" ref={form} onSubmit={sendEmail}>
-            <input placeholder="Your Name" type="name" name="name" />
-            <input placeholder="Your Email" type="email" name="email" />
-            <textarea
-              placeholder="Your Query"
-              rows={8}
-              type="text"
-              name="message"
-            />
-            {/* <div className="contactButton"> */}
-            <Button label="Submit" type="submit" value="Send" />
-            {/* </div> */}
-          </form>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
 
